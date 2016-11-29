a30b669f3a94eb72660ae6a876e5e179138177d8
plelle <philip.lelle@gmail.com>
2016-11-29 18:34:02 -0500

    first push

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'rOcJyRbo3c4WjTldS54s8EyQZ',
+  consumer_secret: 'WHNLf5dpIjfejwkwgmMA5rQBaAEf0fXGhuow7Np4HiOrhq2VcV',
+  access_token_key: '2241079603-oxhZgbgqQGlQK9xIGBgoAjBxTRsuAwwKkKW1rvy',
+  access_token_secret: '8GNnp2vnlmiMwUVJUbMsXVw91orRICz1t3VN4JWwb9bnj',
+}
